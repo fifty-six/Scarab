51e5834d06d66f24a76c397cc7f0ce7beaea4bda
ayugradow <gradow@gmail.com>
2020-02-22 22:38:31 -0300

    Add project files.

---
+++ Modinstaller2/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace Modinstaller2.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
