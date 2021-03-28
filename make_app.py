4863fb28a27659357f466a9b337fdf1f5b098bc3
fifty-six <ybham6@gmail.com>
2021-03-28 15:52:53 -0400

    Fix path issue in Mac .app creation
    Executable was being placed in base directory instead of under
    ModInstaller.app/Contents/MacOS/, so the .app was invalid and showed as
    damaged. We now join the path before writing so that it actually creates
    and uses the directory.

--- make_app.py
+++ make_app.py
@@ -42,7 +42,7 @@ with ZipFile(zip_name, 'w', ZIP_DEFLATED) as zip_f:
         with open(exe, 'rb') as exe_f:
             exe_bytes = exe_f.read()
 
-        info = ZipInfo(exe.name)
+        info = ZipInfo(str(Path(root, mac_os, exe.name)))
         info.date_time = localtime()
         info.external_attr = 0o100755 << 16
         
