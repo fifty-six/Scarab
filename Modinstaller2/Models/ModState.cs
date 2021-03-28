3d25e7d16f6d71210722e8528b6fca7a99c77608
fifty-six <ybham6@gmail.com>
2021-03-27 23:22:04 -0400

    Remove redundant record bodies

--- Modinstaller2/Models/ModState.cs
+++ Modinstaller2/Models/ModState.cs
@@ -1,8 +1,8 @@
 namespace Modinstaller2.Models
 {
-    public abstract record ModState { }
+    public abstract record ModState;
 
     public record InstalledMod(bool Enabled, bool Updated) : ModState;
 
-    public record NotInstalledMod(bool Installing = false) : ModState { }
+    public record NotInstalledMod(bool Installing = false) : ModState;
 }
