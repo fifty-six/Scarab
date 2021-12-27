dc8d75a61753cd9758f08bebe29519b8f35de720
fifty-six <ybham6@gmail.com>
2021-12-27 18:55:45 -0500

    Auto-focus search bar when typing

--- Scarab/Views/ModListView.xaml.cs
+++ Scarab/Views/ModListView.xaml.cs
@@ -1,5 +1,6 @@
 using System.Linq;
 using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Markup.Xaml;
 using JetBrains.Annotations;
 using Scarab.ViewModels;
@@ -9,9 +10,21 @@ namespace Scarab.Views
     [UsedImplicitly]
     public class ModListView : View<ModListViewModel>
     {
+        private readonly TextBox _search;
+
         public ModListView()
         {
             InitializeComponent();
+
+            this.FindControl<UserControl>(nameof(UserControl)).KeyDown += OnKeyDown;
+            
+            _search = this.FindControl<TextBox>("Search");
+        }
+
+        private void OnKeyDown(object? sender, KeyEventArgs e)
+        {
+            if (!_search.IsFocused)
+                _search.Focus();
         }
 
         private void InitializeComponent()
