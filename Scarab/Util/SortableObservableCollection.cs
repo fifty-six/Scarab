9a2318af090b9a3511fb64532358e383fb17de74
fifty-six <ybham6@gmail.com>
2022-01-01 19:37:58 -0500

    Improve performance of SortableObservableCollection

--- Scarab/Util/SortableObservableCollection.cs
+++ Scarab/Util/SortableObservableCollection.cs
@@ -1,22 +1,28 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Reflection;
+using System.Collections.Specialized;
 
 namespace Scarab.Util
 {
+    internal static class EventArgsCache
+    {
+        internal static readonly NotifyCollectionChangedEventArgs ResetCollectionChanged = new (NotifyCollectionChangedAction.Reset);
+    }
+
     internal class SortableObservableCollection<T> : ObservableCollection<T>
     {
         public SortableObservableCollection(IEnumerable<T> iter) : base(iter) {}
         
         public void SortBy(Func<T, T, int> comparer)
         {
+            // This shouldn't ever change due to binary serialization constraints.
             if (Items is not List<T> items)
                 throw new InvalidOperationException("The backing field type is not List<T> on Collection<T>.");
 
             items.Sort((x, y) => comparer(x, y));
-
-            typeof(ObservableCollection<T>).GetMethod("OnCollectionReset", BindingFlags.NonPublic | BindingFlags.Instance)?.Invoke(this, new object[0]);
+            
+            OnCollectionChanged(EventArgsCache.ResetCollectionChanged);
         }
     }
 }
