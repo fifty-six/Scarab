8c35b15d79cd6f50df71f8cc85ba546bba723164
cmot17 <25753364+cmot17@users.noreply.github.com>
2023-07-13 21:28:59 -0400

    feat: make right half of split pane resize with window

--- Scarab/ViewModels/SettingsViewModel.cs
+++ Scarab/ViewModels/SettingsViewModel.cs
@@ -11,9 +11,9 @@ public class SettingsViewModel : ViewModelBase
     public SettingsViewModel()
     {
 #if !DEBUG
-        throw new InvalidOperationException()
+        throw new InvalidOperationException();
 #else
-        
+
         var moq = new Moq.Mock<ISettings>();
         moq.SetupGet(x => x.ManagedFolder).Returns("/home/home/test/Managed");
         Settings = moq.Object;
