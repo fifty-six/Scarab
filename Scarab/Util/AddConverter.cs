8b07f819fb429b205a44868daab4247f121d7d42
Yusuf Bham <ybham6@gmail.com>
2023-08-02 18:35:15 -0400

    refactor: remove unused

--- Scarab/Util/AddConverter.cs
+++
@@ -1,9 +0,0 @@
-using System.Linq;
-using Avalonia.Data.Converters;
-
-namespace Scarab.Util;
-
-public static class AddConverter
-{
-    public static readonly IMultiValueConverter Instance = new FuncMultiValueConverter<double, double>(x => x.Sum());
-}
