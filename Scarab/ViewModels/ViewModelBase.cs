956e426d6bcc9a83bf830696a0ed827274b1dbbf
Yusuf Bham <ybham6@gmail.com>
2023-08-06 13:06:58 -0400

    cleanup: remove Trace from `OnPropertyChanged` and redundant modifiers

--- Scarab/ViewModels/ViewModelBase.cs
+++ Scarab/ViewModels/ViewModelBase.cs
@@ -1,19 +1,16 @@
-using System.Diagnostics;
 using ReactiveUI;
 
 namespace Scarab.ViewModels
 {
     public class ViewModelBase : ReactiveObject
     {
-        protected virtual void RaisePropertyChanged(string name)
+        protected void RaisePropertyChanged(string name)
         {
-            Trace.WriteLine($"Property {name} on type {this.GetType().Name}");
             IReactiveObjectExtensions.RaisePropertyChanged(this, name);
         }
 
-        protected virtual void RaisePropertyChanging(string name)
+        protected void RaisePropertyChanging(string name)
         {
-            Trace.WriteLine($"Property {name} on type {this.GetType().Name}");
             IReactiveObjectExtensions.RaisePropertyChanging(this, name);
         }
     }
