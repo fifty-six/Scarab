92726f1399897457422a01cb90bf06dd47d274d3
Yusuf Bham <ybham6@gmail.com>
2023-08-06 13:13:02 -0400

    fix: revert virtual modifier change for source generator usages

--- Scarab/ViewModels/ViewModelBase.cs
+++ Scarab/ViewModels/ViewModelBase.cs
@@ -1,15 +1,18 @@
+using System.Diagnostics.CodeAnalysis;
 using ReactiveUI;
 
 namespace Scarab.ViewModels
 {
+    // INPC022, INPC031 - Needed for dependencies on base properties
+    [SuppressMessage("ReSharper", "VirtualMemberNeverOverridden.Global")]
     public class ViewModelBase : ReactiveObject
     {
-        protected void RaisePropertyChanged(string name)
+        protected virtual void RaisePropertyChanged(string name)
         {
             IReactiveObjectExtensions.RaisePropertyChanged(this, name);
         }
 
-        protected void RaisePropertyChanging(string name)
+        protected virtual void RaisePropertyChanging(string name)
         {
             IReactiveObjectExtensions.RaisePropertyChanging(this, name);
         }
