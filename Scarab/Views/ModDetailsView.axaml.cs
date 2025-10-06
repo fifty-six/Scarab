f8a0dac65b2c88a8c9ca403714471cd45c18e205
Yusuf Bham <git@fiftysix.dev>
2025-10-06 00:51:08 -0400

    Remove unused debug method

--- Scarab/Views/ModDetailsView.axaml.cs
+++ Scarab/Views/ModDetailsView.axaml.cs
@@ -133,9 +133,4 @@ public partial class ModDetailsView : ReactiveUserControl<ModPageViewModel>
             Log.Error(e, $"{nameof(RepositoryTextClick)} process spawn failed!`");
         }
     }
-
-    private void InputElement_OnPointerPressed(object? sender, PointerPressedEventArgs e)
-    {
-        Log.Debug("sssssssssssssss");
-    }
 }
